f2185a0d7b827e0a7ea2ddc094a70b16211572f6
whynotme1131999 <whynotme1131999@gmail.com>
2023-02-23 16:31:47 +0700

    update code

--- src/routes/web.js
+++ src/routes/web.js
@@ -5,8 +5,8 @@ let router = express.Router();
 
 let initWebRoutes = (app) =>{
     router.get("/",chatbotController.getHomePage);
-    router.get("/webhooks", chatbotController.getWebhook);
-    router.post("/webhooks", chatbotController.postWebhook);
+    router.get("/webhook", chatbotController.getWebhook);
+    router.post("/webhook", chatbotController.postWebhook);
     return app.use("/", router);
 }
 
